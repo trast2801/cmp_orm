5bb9779520afaed3e2e9ddc9290093aff7c0138f
@trast2801 <trast2801@yandex.ru>
2024-08-12 19:23:28 +0300

    на проверку

---
+++ UrbanDjango/task1/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=30)
+    balance =models.DecimalField(decimal_places=2, max_digits=7)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100) # названиеигры
+    cost = models.DecimalField(decimal_places=2, max_digits=7) # цена(DecimalField)
+    size = models.DecimalField(decimal_places=2, max_digits=7) # размер файловигры(DecimalField)
+    description = models.TextField()# описание(неограниченное кол - во  текста)
+    age_limited = models.BooleanField(default= False) # - ограничение возраста 18 + (BooleanField, по умолчанию False)
+    buyer = models.ManyToManyField(Buyer, related_name='buyer')
+
+    def __str__(self):
+        return self.title
